df8b8d090a63190dd1b7be5795784b0dd8b7957a
vishal <vishalmote@gmail.com>
2020-02-11 07:38:05 +0530

    default admin seeder, laravel/ui package addition, bootstrap theme enabling, db configuration in env

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,27 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UsersTableSeeder::class);
+        $email = 'admin@admin.com';
+        if (User::where('email', $email)->count())
+            return;
+        $usr = new User();
+        $usr->name = 'Administrator';
+        $usr->email = $email;
+        $usr->password = Hash::make('password');
+        $usr->email_verified_at = date('Y-m-d H:i:s');
+        $usr->save();
     }
+
 }
